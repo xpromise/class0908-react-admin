4de4feff77cb9475ff013c7bf9079dd968b1396b
xiongjian <webjsforyou@163.com>
2020-01-11 16:49:09 +0800

    完成Login的表单校验功能2

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # 硅谷后台管理项目
+
+## git 操作
+
+- 项目经理（技术总监）
+  _ 创建仓库
+  _ 创建初始化项目 \* 初始化项目提交到仓库中保管
+
+- 开发者
+  _ 获取仓库代码
+  _ https
+  _ 项目经理提供：仓库地址 和 用户名/密码
+  _ ssh
+  _ 项目经理提供：仓库地址
+  _ 开发者提供：ssh pub（公钥）
+  _ git clone 仓库地址
+  _ 选择分支
+  _ 项目经理帮你创建 xxx 分支
+  _ git fetch origin xxx:xxx 拉取远程分支 xxx 到本地分支 xxx 上
+  _ git checkout xxx 切换到 xxx 分支
+  _ 进行开发
+  _ 项目经理没有帮你创建
+  _ git checkout -b xxx 创建并切换到 xxx 分支（将当前分支内容复制到 xxx 分支上）
+  _ 进行开发
+  _ 进行开发
+  _ 本地版本控制
+  _ git add .
+  _ git commit -m 'xxx'
+  _ 提交到远程仓库
+  _ git push origin xxx
+  _ 有可能提交失败
+  _ 网络问题 （等等试试）
+  _ 本地没有进行版本控制 \* 远程仓库有更新 git pull origin xxx
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -8,6 +8,36 @@ import './index.less';
 
 @Form.create()
 class Login extends Component {
+  // 自定义表单校验规则
+  validator = (rule, value, callback) => {
+    /*
+      rule.field 获取表单key
+      value 获取表单value
+    */
+    // console.log(rule, value);
+
+    const name = rule.field === 'username' ? '用户名' : '密码';
+
+    const reg = /^\w+$/;
+
+    if (!value) {
+      // 输入值为空
+      callback(`${name}不能为空`);
+    } else if (value.length < 4) {
+      callback(`${name}必须大于4位`);
+    } else if (value.length > 15) {
+      callback(`${name}必须小于15位`);
+    } else if (!reg.test(value)) {
+      callback(`${name}只能包含英文、数字、下划线`);
+    }
+    /*
+      callback() 调用不传参，代表表单校验成功
+      callback(message) 调用传参，代表表单校验失败，会提示message错误
+    */
+    // 必须要调用，否则会出问题
+    callback();
+  };
+
   render() {
     // getFieldDecorator 高阶组件：用来表单校验
     const { getFieldDecorator } = this.props.form;
@@ -24,7 +54,7 @@ class Login extends Component {
             <Form.Item>
               {getFieldDecorator('username', {
                 rules: [
-                  {
+                  /* {
                     required: true,
                     message: '用户名不能为空'
                   },
@@ -37,8 +67,12 @@ class Login extends Component {
                     message: '用户名必须小于15位'
                   },
                   {
-                    pattern: /^\w$/,
+                    pattern: /^\w+$/,
                     message: '用户名只能包含英文、数字、下划线'
+                  } */
+
+                  {
+                    validator: this.validator
                   }
                 ]
               })(
@@ -51,12 +85,20 @@ class Login extends Component {
               )}
             </Form.Item>
             <Form.Item>
-              <Input
-                prefix={
-                  <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
-                }
-                placeholder='密码'
-              />
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    validator: this.validator
+                  }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  placeholder='密码'
+                />
+              )}
             </Form.Item>
             <Form.Item>
               <Button className='login-form-btn' type='primary'>
