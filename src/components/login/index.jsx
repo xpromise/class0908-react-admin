ea9fa7108fe07d6a95b3417e3b1d0c9e3bd2c14d
xiongjian <webjsforyou@163.com>
2020-01-11 16:14:51 +0800

    完成Login的表单校验功能1

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,75 @@
 import React, { Component } from 'react';
 
-export default class Login extends Component {
+import { Form, Input, Button, Icon } from 'antd';
+
+// 图片必须引入，才会被webpack打包
+import logo from './logo.png';
+import './index.less';
+
+@Form.create()
+class Login extends Component {
   render() {
-    return <div>Login...</div>;
+    // getFieldDecorator 高阶组件：用来表单校验
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div className='login'>
+        <header className='login-header'>
+          <img src={logo} alt='logo' />
+          <h1>React项目: 后台管理系统</h1>
+        </header>
+        <section className='login-section'>
+          <h3>用户登录</h3>
+          <Form className='login-form'>
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    message: '用户名不能为空'
+                  },
+                  {
+                    min: 4,
+                    message: '用户名必须大于3位'
+                  },
+                  {
+                    max: 15,
+                    message: '用户名必须小于15位'
+                  },
+                  {
+                    pattern: /^\w$/,
+                    message: '用户名只能包含英文、数字、下划线'
+                  }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  placeholder='用户名'
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Input
+                prefix={
+                  <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
+                }
+                placeholder='密码'
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button className='login-form-btn' type='primary'>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
+    );
   }
 }
+
+// Form.create()(Login) 高阶组件：给Login传递form属性
+// export default Form.create()(Login);
+export default Login;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
+import './index.less';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
